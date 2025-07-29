63185411e1e7db09f40f07560b5c02193c22de54
unknown <vanjordan46@hotmail.com>
2025-07-29 16:38:08 -0700

    Jobs API

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -3,6 +3,10 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,6 +15,9 @@ app.use(express.json());
 // extra packages
 
 // routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter)
+
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
@@ -22,6 +29,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const {StatusCodes} = require("http-status-codes");
+
+
+
+
+const register = async(req,res)=>{
+
+  
+  // if(!name || !email || !password){
+  //   throw new BadRequestError('Please provide name, email, and password')
+  // }
+  const user = await User.create({...req.body})
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
+}
+
+const login = async(req,res)=>{
+  res.send("login user")
+}
+
+module.exports = {
+  register, login
+}
